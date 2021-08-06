de7e215d288eb63b698c55edaa72cfb3c246c6bf
Inian <inian1234@gmail.com>
2021-08-06 23:44:38 +0800

    use card component instead

--- frontend/components/Podcast/PodcastEpisode.js
+++ frontend/components/Podcast/PodcastEpisode.js
@@ -1,18 +1,15 @@
-import { useEffect, useState } from "react";
 import { Typography } from "@supabase/ui";
+import { Card } from "@supabase/ui";
 
 const PodcastEpisode = ({ title, description, url, type }) => {
+  let cover;
+  if (type == "video/mp4") cover = <video src={url} controls />;
+  else cover = <audio src={url} controls />;
   return (
     <>
-      <div className="flex w-full justify-center">
-        {type === "video/mp4" && <video width="300" src={url} controls />}
-        {type === "audio/mp3" && <audio width="300" src={url} controls />}
-      </div>
-
-      <Typography.Title level={3}>{title}</Typography.Title>
-      <Typography.Text>{description}</Typography.Text>
-      <br />
-      <br />
+      <Card title={title} cover={cover}>
+        <Typography.Text>{description}</Typography.Text>
+      </Card>
     </>
   );
 };
